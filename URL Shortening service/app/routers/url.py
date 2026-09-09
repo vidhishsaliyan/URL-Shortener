9f3bad1cb11480cf0754bb1137511db4e66ca021
Vidhish <videeshsalian7@gmail.com>
2026-09-10 02:36:12 +0530

    removed reachabiliy check

--- URL Shortening service/app/routers/url.py
+++ URL Shortening service/app/routers/url.py
@@ -4,7 +4,6 @@ from ..schemas import urlSchema
 from ..models import urlModel
 from ..database import get_db
 from sqlalchemy.orm import Session
-from ..utils.shorten_utils import is_url_reachable
 router = APIRouter(prefix="/api/urls", tags=["URL Shortening"])
 
 ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -21,12 +20,6 @@ def base62_encode(number):
 
 @router.post('/shorten', status_code=status.HTTP_201_CREATED)
 async def create_short_url(request: Request, request_body : urlSchema.RequestURL, db: Session = Depends(get_db)):
-    long_url = str(request_body.long_url)
-
-    reachable = await is_url_reachable(long_url)
-    if not reachable:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Invalid URL')
-
     expiry_minutes = 30
     expiry_time = datetime.now(timezone.utc) + timedelta(minutes=expiry_minutes)
     try:
@@ -37,6 +30,7 @@ async def create_short_url(request: Request, request_body : urlSchema.RequestURL
     SPACE = 62**6
     value = (value*26860463)%SPACE
     short_url = base62_encode(value)
+    long_url = str(request_body.long_url)
     user_id_header = request.headers.get("X-User-ID")
     new_entry = urlModel.URL(long_url=long_url, short_url=str(short_url), expiry_time=expiry_time, user_id=user_id_header)
     db.add(new_entry)
--- URL Shortening service/app/utils/shorten_utils.py
+++
@@ -1,9 +0,0 @@
-import httpx
-async def is_url_reachable(url: str):
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.head(url=url, timeout=5)
-            print(response)
-            return response.status_code < 400
-    except:
-        return False
